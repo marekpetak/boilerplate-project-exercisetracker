7ee6ffb3621e183115de4fa71636ac447969cc47
mattko <marekpetak@users.noreply.github.com>
2018-09-27 12:58:23 +0100

    new server

--- server.js
+++ server.js
@@ -1,11 +1,29 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
+const shortid = require('shortid');
 
 const cors = require('cors')
 
 const mongoose = require('mongoose')
 mongoose.connect(process.env.MLAB_URI || 'mongodb://localhost/exercise-track' )
+const userSchema = mongoose.Schema({
+  username: String,
+  excersizes: [
+    {
+      description: {
+        type: String,
+        required: true
+      },
+      duration: {
+        type: Number,
+        required: true
+      },
+      date: Date
+    }
+  ]
+});
+var User = mongoose.model('User', userSchema);
 
 app.use(cors())
 
@@ -17,6 +35,105 @@ app.use(express.static('public'))
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
+
+// POST create user
+app.post('/api/exercise/new-user', (req, res) => {
+  // body: username=marek return username & _id
+  User.findOne({username: req.body.username}, (error, data) => {
+    if (error) {
+      console.log(error);
+    } else {
+      if (data) { 
+        res.json({'warning': 'User already exists'});  
+      } else {
+       User.create({username: req.body.username}, (error, data) => { 
+         if (error) {
+           console.log(error);
+         } else {
+           res.json(data);
+         }
+       });
+      }
+    }
+  });
+});
+
+// POST add excersise to user by _id
+// username	"marek"
+// description	"swimming"
+// duration	30
+// _id	"Bky07uLtQ"
+// date	"Fri Sep 05 2008"
+app.post('/api/exercise/add', (req, res) => {
+  User.findById(req.body.userId, (error, data) => {
+    if (error) {
+     console.log(error); 
+    } else {
+      console.log('body', req.body);
+      console.log(data);
+      data.excersizes.push({description: req.body.description, duration: req.body.duration, date: new Date(req.body.date) || new Date() }); 
+      data.save((error, data) => {
+       if (error) {
+         console.log(error);
+       } else {
+         res.json(data);
+       }
+     });
+    }
+  });
+});
+
+// GET get all users
+app.get('/api/exercise/users', (req, res) => {
+  User.find({}, (error, data) => {
+    if (error) {
+      console.log(error);
+    } else {
+      res.json(data);
+    }
+  })
+});
+
+// GET user excersize log
+app.get('/api/exercise/log', (req, res) => {
+  User.findById(req.query.userId).exec((error, data) => {
+    if (error) {
+      console.log(error);
+    } else {
+      const resObj = {
+        "_id": data._id,
+        "username": data.username
+      }
+      
+      let result;
+      if (req.query.from) {
+        resObj['from'] = new Date(req.query.from);
+        result = data.excersizes.filter((item) => {
+          return (new Date(item.date)) > (new Date(req.query.from));
+        });
+        
+        data.excersizes = result;
+      }
+      
+      if (req.query.to) {
+        resObj['to'] = new Date(req.query.to);
+        result = data.excersizes.filter((item) => {
+          return (new Date(item.date)) < (new Date(req.query.to));
+        });
+        
+        data.excersizes = result;
+      }
+      
+      resObj['count'] = data.excersizes.length;
+      resObj['excersizes'] = data.excersizes;
+      
+      
+      res.json(resObj);
+    }
+  });
+  
+});
+
 
 
 // Not found middleware
